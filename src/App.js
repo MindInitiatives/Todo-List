1d079b8ea214d5dda853a57e33c1ed6ba89023e9
MindInitiatives <stevyn258@yahoo.com>
2019-08-28 18:02:44 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
-function App() {
+class App extends Component {
+  
+  state = {
+    todos: [
+      {
+        id:1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id:2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id:3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
+  // Toggle Complete
+  markComplete = (id) => {
+       this.setState({ todos: this.state.todos.map(todo => {
+         if(todo.id === id) {
+           todo.completed = !todo.completed
+         }
+         return todo
+       })});
+  }
+
+  //Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !==id)]});
+  }
+
+  //Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]});
+  }
+
+    render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header/>
+      <AddTodo addTodo={this.addTodo}/>
+      <Todos todos={this.state.todos} markComplete={this.markComplete}
+      delTodo={this.delTodo}/></div>
+      
     </div>
   );
+}
 }
 
 export default App;
